aa18824fa2fec5fce147552f6c68f4d28901b2c1
Dmitri Lebed <lebed.40221@gmail.com>
2025-01-19 14:30:43 +0300

    v0.5.2

--- patient/main.py
+++ patient/main.py
@@ -1,127 +1,99 @@
-# Function to read a CSV file
-def read_csv(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            content = file.read()
-            rows = content.split("\n")
-            data = [row.split(",") for row in rows if row]  # Split rows into columns
-            return data
-    except FileNotFoundError:
-        return []
+import engine
+
+PATIENTS_FILE_NAME = engine.FILE_NAMES["PATIENTS"]
+SERVICES_FILE_NAME = engine.FILE_NAMES["SERVICES"]
 
 
-# Function to write to a CSV file
-def write_csv(file_name, data):
-    try:
-        with open(file_name, 'w') as file:
-            for row in data:
-                file.write(",".join(row) + "\n")
-    except Exception as e:
-        print(f"Error writing to file: {e}")
-
-
-# Function to append to a CSV file
-def append_to_csv(file_name, data):
-    try:
-        with open(file_name, 'a') as file:
-            file.write(",".join(data) + "\n")
-    except Exception as e:
-        print(f"Error appending to file: {e}")
+def create_appointment(patient_id):
+    date, doctor_id = input("Enter appointment details (date, doctor id): ").split(", ")
+    doctors = [d for d in engine.csv_read(engine.FILE_NAMES["DOCTORS"]) if doctor_id == d["id"]]
+    if not doctors:
+        raise ValueError("Doctor with this id does not exist")
+    description = input("Enter appointment description: ")
+    return {
+        "doctor_id": doctor_id,
+        "patient_id": patient_id,
+        "date": date,
+        "description": description,
+        "status": "payment_pending",
+        "is_appointment": True
+    }
 
 
 # Allows user to access and view personal medical records
 def view_medical_records(patient_id):
-    diagnosis_file = f"{patient_id}_diagnosis.csv"
-    prescription_file = f"{patient_id}_prescription.csv"
-
     print("\n--- Medical Records ---")
 
-    # Load diagnosis and prescription
-    diagnosis = read_csv(diagnosis_file)
-    prescription = read_csv(prescription_file)
-
-    print("Diagnosis:")
-    if diagnosis:
-        for row in diagnosis:
-            print(", ".join(row))
-    else:
-        print("No diagnosis records found.")
-
-    print("\nPrescription:")
-    if prescription:
-        for row in prescription:
-            print(", ".join(row))
-    else:
-        print("No prescription records found.")
+    patients = [p for p in engine.csv_read(PATIENTS_FILE_NAME) if p["id"] == patient_id]
+    for patient in patients:
+        print(f"Diagnosis: {patient['diagnosis']}, Prescription: {patient['diagnosis']}, Treatment Plan: {patient['treatment_plan']}")
 
 
 # Function to view appointments
-def view_appointments(patient_id):
-    appointments_file = f"{patient_id}_appointments.csv"
+def view_appointments():
+    doctors_file = engine.FILE_NAMES["DOCTORS"]
 
     print("\n--- Appointments ---")
-    appointments = read_csv(appointments_file)
-    if appointments:
-        print("Upcoming Appointments:")
-        for row in appointments:
-            print(", ".join(row))
-    else:
-        print("No upcoming appointments.")
+    services = engine.csv_read(SERVICES_FILE_NAME)
+    doctors = engine.csv_read(doctors_file)
+
+    for service in services:
+        if service['is_appointment']:
+            doctor = [d for d in doctors if d["id"] == service["doctor_id"]][0]
+            print(f"Doctor: {doctor['name']}, Date: {service['date']}, Description: {service['description']}, Status: {service['status']}")
 
 
 # Function to schedule an appointment
 def schedule_appointment(patient_id):
-    appointments_file = f"{patient_id}_appointments.csv"
-
     try:
         # Ask user if they want to schedule a new appointment
         schedule_choice = input("\nWould you like to schedule a new appointment? (yes/no): ").lower()
         if schedule_choice == 'yes':
             # Schedule a new appointment
-            appointment_details = input("Enter appointment details (date, time, doctor): ").split(", ")
-            append_to_csv(appointments_file, appointment_details)
+
+            new_appointment = create_appointment(patient_id)
+            service_id = engine.csv_write_service(new_appointment)
+            new_billing = {"patient_id": patient_id, "service_id": service_id, "total": 600, "paid": 0, "date": new_appointment["date"]}
+            engine.csv_write_billing(new_billing)
             print("Appointment scheduled successfully!")
         elif schedule_choice == 'no':
             print("No new appointment scheduled.")
         else:
             print("Invalid choice. Returning to the main menu.")
-    except (ValueError, KeyboardInterrupt):
-        print("\nError: Invalid input. Please enter a valid choice.")
+    except ValueError as e:
+        print(e)
 
 
 # Main function to view and schedule appointments
 def view_and_schedule_appointments(patient_id):
-    view_appointments(patient_id)  # View existing appointments
+    view_appointments()  # View existing appointments
     schedule_appointment(patient_id)  # Schedule a new appointment if the user chooses to do so
 
 
 # Allows user to check and update personal information
 def update_personal_info(patient_id):
-    info_file = f"{patient_id}_info.csv"
-
     # Load current personal information
-    personal_info = read_csv(info_file)
+    patients = engine.csv_read(PATIENTS_FILE_NAME)
+    patient = [p for p in patients if patient_id == p["id"]][0]
 
     print("\n--- Current Personal Information ---")
-    if personal_info:
-        for row in personal_info:
-            print(", ".join(row))
-    else:
-        print("No personal information found.")
+    print(f"Name: {patient['name']}, Age: {patient['age']}, Phone number: {patient['phone_number']}, Address: {patient['address']}")
 
     try:
         # Asks user if they want to update their personal information
         update_choice = input("\nWould you like to update your personal information? (yes/no): ").lower()
 
         if update_choice == 'yes':
-            print("\nEnter new information (Address, Contact Number, Insurance Details):")
+            print("\nEnter new information (Name, Age, Address, Contact Number): ")
+            name = input("Name: ")
+            age = input("Age: ")
+            number = input("Number: ")
             address = input("Address: ")
-            contact_number = input("Contact Number: ")
-            insurance_details = input("Insurance Details: ")
 
-            new_info = [address, contact_number, insurance_details]
+            patient.update({"name": name, "age": age, "address": address, "number": number})
+            patients_new = [patient if p["id"] == patient_id else p for p in patients]
+            engine.csv_rewrite(PATIENTS_FILE_NAME, patients_new)
 
-            write_csv(info_file, [new_info])
             print("Personal information updated successfully!")
         elif update_choice == 'no':
             print("No changes made to your personal information.")
@@ -133,51 +105,26 @@ def update_personal_info(patient_id):
 
 # Allows user to request specific services
 def request_service(patient_id):
-    services_file = f"{patient_id}_services.csv"
-
-    print("\n--- Request Specific Services ---")
-    print("1. Follow-up Appointment")
-    print("2. Request specific doctor")
-
+    print("\n--- Request follow-up appointment with specific doctor ---")
     try:
-        service_choice = input("Enter your choice (1/2): ")
-
-        if service_choice == '1':
-            service = "Follow-up Appointment"
-        elif service_choice == '2':
-            doctor_name = input("Enter doctor's name: ")
-            service = f"Request specific doctor: {doctor_name}"
-        else:
-            print("Invalid choice. No service requested.")
-            return
-
-        append_to_csv(services_file, [service])
-        print(f"Service '{service}' requested successfully!")
+        follow_up_appointment = create_appointment(patient_id)
+        service_id = engine.csv_write_service(follow_up_appointment)
+        new_billing = {"patient_id": patient_id, "service_id": service_id, "total": 600, "paid": 0, "date": follow_up_appointment["date"]}
+        engine.csv_write_billing(new_billing)
+        print("Follow-up appointment was scheduled successfully!")
     except (ValueError, KeyboardInterrupt):
         print("\nError: Invalid input. Please enter a valid choice.")
 
 
 # Allows user to view billing details
 def view_billing_details(patient_id):
-    billing_file = f"{patient_id}_billing.csv"
+    billing_file = engine.FILE_NAMES["BILLING"]
 
     print("\n--- Billing Details ---")
-    billing_details = read_csv(billing_file)
-    if billing_details:
-        for row in billing_details:
-            print(", ".join(row))
-    else:
-        print("No billing details found.")
-
-    # View payment history
-    payment_history_file = f"{patient_id}_payment_history.csv"
-    print("\nPayment History:")
-    payment_history = read_csv(payment_history_file)
-    if payment_history:
-        for row in payment_history:
-            print(", ".join(row))
-    else:
-        print("No payment history found.")
+    for billing in [b for b in engine.csv_read(billing_file) if b["patient_id"] == patient_id]:
+        service_description = [s for s in engine.csv_read(SERVICES_FILE_NAME) if s["id"] == billing["service_id"]][0]["description"]
+        patient_name = [p for p in engine.csv_read(PATIENTS_FILE_NAME) if p["id"] == billing["patient_id"]][0]["name"]
+        print(f"Patient Name: {patient_name}, Description: {service_description}, Total: {billing['total']}, Paid: {billing['paid']}, Date: {billing['date']}")
 
 
 # Main menu function
@@ -185,8 +132,11 @@ def main():
     print("Welcome to the Hospital Management System Patient Menu")
     try:
         patient_id = input("Please enter your patient ID: ")
-    except (ValueError, KeyboardInterrupt):
-        print("\nError: Invalid input. Exiting system.")
+        patient = [p for p in engine.csv_read(PATIENTS_FILE_NAME) if patient_id == p["id"]]
+        if not patient:
+            raise ValueError("Error. Patient with this id does not exist")
+    except ValueError as e:
+        print(e)
         return
 
     while True:
